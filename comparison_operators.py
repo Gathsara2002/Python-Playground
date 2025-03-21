51690c0ee55b2885448e59da25fed49ba4fdba4d
Gathsara2002 <liviru2002mmg@gmail.com>
2025-03-21 18:54:51 +0530

    comparison operators

---
+++ comparison_operators.py
@@ -0,0 +1,22 @@
+# Comparison operators are used to compare two values
+
+x = 5
+y = 3
+
+# Equal
+print(x == y)
+
+# Not equal
+print(x != y)
+
+# Greater than
+print(x > y)
+
+# Lower than
+print(x < y)
+
+# Greater than or equal
+print(x >= y)
+
+# Lower than or equal
+print(x <= y)
