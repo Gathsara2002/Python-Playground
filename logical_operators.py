974d64965a39fe461b6d2275e969fa92f56e849c
Gathsara2002 <liviru2002mmg@gmail.com>
2025-03-21 19:03:20 +0530

    logical operators

---
+++ logical_operators.py
@@ -0,0 +1,17 @@
+# Logical operators are used to combine conditional statements
+
+# Logical AND
+print(True and True)  # True
+print(True and False)  # False
+print(False and True)  # False
+print(False and False)  # False
+
+# Logical OR
+print(True or True)  # True
+print(True or False)  # True
+print(False or True)  # True
+print(False or False)  # False
+
+# Logical NOT
+print(not True)  # False
+print(not False)  # True
