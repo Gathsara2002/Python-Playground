b6c13a54f331a68107a6f7f643662e019d812a68
Gathsara2002 <liviru2002mmg@gmail.com>
2025-03-21 18:40:49 +0530

    arithmetic operators

---
+++ arithmetic_operators.py
@@ -0,0 +1,33 @@
+# Arithmetic operators are used with numeric values to perform common mathematical operations
+
+x = 10
+y = 3
+
+# Addition
+print(x + y)  # 13
+
+# Substraction
+print(x - y)  # 7
+
+# Multiplication
+print(x * y)  # 30
+
+# Division
+print(x / y)  # 3.333..
+
+# Floor Division - returns the largest integer less than or equal to the result of the division
+print(x // y)  # 3
+
+# Modulus - return reminder after devision
+print(x % y)  # 1
+
+# Power
+print(x**y)  # 1000
+
+"""
+If equation has two or more operation you have to solve using operator precedence.
+()
+**
+*,/,//,%
++,-
+"""
