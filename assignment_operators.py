d2f36b172f7151772a2542d2b73ece9ec30acad2
Gathsara2002 <liviru2002mmg@gmail.com>
2025-03-21 18:50:24 +0530

    assignmnet operators

---
+++ assignment_operators.py
@@ -0,0 +1,33 @@
+# Assignment operators are used to assign values to variables
+
+x = 5
+y = x
+print(y)  # 5
+
+x = 5
+x += 5  # Same as x = x + 5
+print(x)  # 10
+
+x = 5
+x -= 3  # Same as x = x - 3
+print(x)
+
+x = 5
+x *= 2  # Same as x = x * 2
+print(x)
+
+x = 5
+x /= 2  # Same as x = x / 2
+print(x)
+
+x = 5
+x //= 3  # Same as x = x // 3
+print(x)
+
+x = 5
+x %= 4  # Same as x = x % 4
+print(x)
+
+x = 5
+x **= 2  # Same as x = x ** 2
+print(x)
